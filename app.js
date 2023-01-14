25fb74c51b9fe177f0f19ddae822e343aa211552
Marcus Heisters <marcus.heisters@googlemail.com>
2023-01-14 10:46:27 +0100

    Save posts in DB

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+const port = process.env.PORT || 3000;
+const config = require("./config");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,48 +14,64 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+// DB and mongoose
+mongoose.set('strictQuery', false);
+const db = mongoose.connect(config.DB_CONNECTION + "/blogDB");
+console.log("Succesfully connected to db /BlogWebsite");
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+// Schemas 
+const postSchema = {title: String, content: String};
+const Post = mongoose.model("Post", postSchema); 
 
 let posts = [];
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
-    });
+  });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", (req, res) => {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
 
+  const postItem = new Post({
+    title: post.title,
+    content: post.content
+  });
+
+  postItem.save();
   posts.push(post);
 
   res.redirect("/");
 
 });
 
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postName", function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
+  posts.forEach(function (post) {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
@@ -65,6 +84,6 @@ app.get("/posts/:postName", function(req, res){
 
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
